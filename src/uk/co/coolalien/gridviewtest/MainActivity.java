4beec8f93a9e152f607b9eba101ecedf2e257c6a
Daniel Stammers <stammers.daniel@gmail.com>
2013-07-06 20:45:05 +0100

    Added progress bar on intial download

--- src/uk/co/coolalien/gridviewtest/MainActivity.java
+++ src/uk/co/coolalien/gridviewtest/MainActivity.java
@@ -8,10 +8,12 @@ import java.util.HashMap;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -30,7 +32,7 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	public final static String EXTRA_MESSAGE = "FILM";
-	
+
 	ArrayList<Drawable> images = new ArrayList<Drawable>();
 	ArrayList<String> imageNames = new ArrayList<String>();
 	ArrayList<Film> films;
@@ -39,126 +41,200 @@ public class MainActivity extends Activity {
 	FilmHarvester harvester;
 	ImageAdapter adapter;
 	private GuiThreadMessageHandler guiThreadMsgHandler;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        me = this;
-       harvester = new FilmHarvester(me);
-       guiThreadMsgHandler = new GuiThreadMessageHandler();
-        Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run() {
-                try {
-                	 
-                     films = harvester.loadFilms();
-                     //for(int i = 0; i < films.size(); i++){
-                    //	 String image = films.get(i).getImage();
-                    //	 String name = image.substring(image.lastIndexOf("/")+1, image.length());
-                    //	 filmPosition.put(name, i);
-                    // }
 
-                     
-                     	for(int i = 0; i < films.size(); i++){
-                     		
-                     		//store images in correct order
-                     		String image = films.get(i).getImage();
-                     		String imageName = image.substring(image.lastIndexOf("/")+1, image.length());
-                     		// get input stream
-                             //InputStream ims = manager.open("films/" + imageName);
-                     		File imageFile = new File(getFilesDir(), imageName);
-                             // load image as Drawable
-                             Drawable d = Drawable.createFromPath(imageFile.getPath());
-                             images.add(d);
-                             imageNames.add(imageName);
-                             //ims.close();
-                     	}
-                        
-                     Message msg = Message.obtain(guiThreadMsgHandler);
-                     msg.sendToTarget();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        
-        thread.start();
-        
-        Log.d("Debugger", String.valueOf(images.size()));
-        GridView gridView = (GridView) findViewById(R.id.gridview);
-        adapter = new ImageAdapter(this);
-        gridView.setAdapter(adapter);
- 
-        gridView.setOnItemClickListener(new OnItemClickListener() 
-        {
-            public void onItemClick(AdapterView<?> parent, 
-            View v, int position, long id) 
-            {    
-            	if(films != null){
-            		Intent intent = new Intent(me, FilmActivity.class);
-            		Bundle b = new Bundle();
-            		b.putSerializable(EXTRA_MESSAGE, films.get(position));
-            		intent.putExtras(b);
-            		startActivity(intent);
-            		//Log.d("Debugging message", imageNames.get(position));
-            		//Log.d("Debugging message", String.valueOf(filmPosition.get(imageNames.get(position))));
-            		//Toast.makeText(getBaseContext(), 
-                    //        films.get(position).getName() + " selected", 
-                    //        Toast.LENGTH_SHORT).show();
-            	}else{
-            		
-            	}
-                
-            }
-        });   
-        Log.d("Debugger", "Done");
-        
-    }
-    
-    public class ImageAdapter extends BaseAdapter {
-        private Context context;
- 
-        public ImageAdapter(Context c) {
-            context = c;
-        }
- 
-        //---returns the number of images---
-        public int getCount() {
-            return images.size();
-        }
- 
-        //---returns the ID of an item--- 
-        public Object getItem(int position) {
-            return images.get(position);
-        }
- 
-        public long getItemId(int position) {
-            return position;
-        }
- 
-        //---returns an ImageView view---
-        public View getView(int position, View convertView, ViewGroup parent) {
-            ImageView imageView;
-            if (convertView == null) {
-                imageView = new ImageView(context);
-                imageView.setLayoutParams(new GridView.LayoutParams(220, 330));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(0, 0, 0, 0);
-            } else {
-                imageView = (ImageView) convertView;
-            }
-            imageView.setImageDrawable(images.get(position));
-            return imageView;
-        }
-    }    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		me = this;
+		harvester = new FilmHarvester(me);
+		guiThreadMsgHandler = new GuiThreadMessageHandler();
+		Thread thread = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				try {
 
-    public class GuiThreadMessageHandler extends Handler {
-        public void handleMessage(Message m){
-            adapter.notifyDataSetChanged();
-        }
-    }
+					films = harvester.loadFilms();
+					//for(int i = 0; i < films.size(); i++){
+					//	 String image = films.get(i).getImage();
+					//	 String name = image.substring(image.lastIndexOf("/")+1, image.length());
+					//	 filmPosition.put(name, i);
+					// }
 
-    
+
+					for(int i = 0; i < films.size(); i++){
+
+						//store images in correct order
+						String image = films.get(i).getImage();
+						String imageName = image.substring(image.lastIndexOf("/")+1, image.length());
+						// get input stream
+						//InputStream ims = manager.open("films/" + imageName);
+						File imageFile = new File(getFilesDir(), imageName);
+						// load image as Drawable
+						Drawable d = Drawable.createFromPath(imageFile.getPath());
+						images.add(d);
+						imageNames.add(imageName);
+						//ims.close();
+					}
+
+					Message msg = Message.obtain(guiThreadMsgHandler);
+					msg.sendToTarget();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		//thread.start();
+		new LoadingInfo().execute("GO");
+		Log.d("Debugger", String.valueOf(images.size()));
+		GridView gridView = (GridView) findViewById(R.id.gridview);
+		adapter = new ImageAdapter(this);
+		gridView.setAdapter(adapter);
+
+		gridView.setOnItemClickListener(new OnItemClickListener() 
+		{
+			public void onItemClick(AdapterView<?> parent, 
+					View v, int position, long id) 
+			{    
+				if(films != null){
+					Intent intent = new Intent(me, FilmActivity.class);
+					Bundle b = new Bundle();
+					b.putSerializable(EXTRA_MESSAGE, films.get(position));
+					intent.putExtras(b);
+					startActivity(intent);
+					//Log.d("Debugging message", imageNames.get(position));
+					//Log.d("Debugging message", String.valueOf(filmPosition.get(imageNames.get(position))));
+					//Toast.makeText(getBaseContext(), 
+					//        films.get(position).getName() + " selected", 
+					//        Toast.LENGTH_SHORT).show();
+				}else{
+
+				}
+
+			}
+		});   
+		Log.d("Debugger", "Done");
+
+	}
+
+	public class ImageAdapter extends BaseAdapter {
+		private Context context;
+
+		public ImageAdapter(Context c) {
+			context = c;
+		}
+
+		//---returns the number of images---
+		public int getCount() {
+			return images.size();
+		}
+
+		//---returns the ID of an item--- 
+		public Object getItem(int position) {
+			return images.get(position);
+		}
+
+		public long getItemId(int position) {
+			return position;
+		}
+
+		//---returns an ImageView view---
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView;
+			if (convertView == null) {
+				imageView = new ImageView(context);
+				imageView.setLayoutParams(new GridView.LayoutParams(220, 330));
+				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+				imageView.setPadding(0, 0, 0, 0);
+			} else {
+				imageView = (ImageView) convertView;
+			}
+			imageView.setImageDrawable(images.get(position));
+			return imageView;
+		}
+	}    
+
+	public class GuiThreadMessageHandler extends Handler {
+		public void handleMessage(Message m){
+			adapter.notifyDataSetChanged();
+		}
+	}
+
+	public class LoadingInfo extends AsyncTask<String, Integer, String> {
+
+		ProgressDialog progressBar;
+
+		@Override
+		protected String doInBackground(String... params) {
+			films = harvester.loadFilms();
+			//for(int i = 0; i < films.size(); i++){
+			//	 String image = films.get(i).getImage();
+			//	 String name = image.substring(image.lastIndexOf("/")+1, image.length());
+			//	 filmPosition.put(name, i);
+			// }
+
+
+			for(int i = 0; i < films.size(); i++){
+
+				//store images in correct order
+				String image = films.get(i).getImage();
+				String imageName = image.substring(image.lastIndexOf("/")+1, image.length());
+				// get input stream
+				//InputStream ims = manager.open("films/" + imageName);
+				File imageFile = new File(getFilesDir(), imageName);
+				// load image as Drawable
+				Drawable d = Drawable.createFromPath(imageFile.getPath());
+				images.add(d);
+				imageNames.add(imageName);
+				float percentage = ((i+1)/films.size()) * 100;
+				publishProgress((int)Math.ceil(percentage));
+				//ims.close();
+			}
+			publishProgress(100);
+			return null;
+		}
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
+		 */
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			//super.onPostExecute(result);
+			progressBar.dismiss();
+			adapter.notifyDataSetChanged();
+		}
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#onPreExecute()
+		 */
+		@Override
+		protected void onPreExecute() {
+			progressBar = new ProgressDialog(MainActivity.this);
+			//progressBar.setCancelable(true);
+			progressBar.setMessage("File downloading ...");
+			progressBar.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+			progressBar.setProgress(0);
+			progressBar.setMax(100);
+			progressBar.show();
+			//progressBar.show(MainActivity.this, "Progress Dialog Title Text","Process Description Text", true);
+		}
+
+		/* (non-Javadoc)
+		 * @see android.os.AsyncTask#onProgressUpdate(Progress[])
+		 */
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			// TODO Auto-generated method stub
+			//super.onProgressUpdate(values);
+			//Toast.makeText(getApplicationContext(), "Loaded "+ values[0], Toast.LENGTH_LONG).show();
+			progressBar.setProgress(values[0]);
+
+		}
+
+	}
+
+
 }
 
